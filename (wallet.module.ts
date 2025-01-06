57fef32ec435ff138f6eafc929bd31a3c9ae68dc
Kapten boneng <iwak01bogo@gmail.com>
2025-01-06 19:28:54 +0700

    Create (wallet.module.ts

---
+++ (wallet.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { WalletController } from './wallet.controller';
+import { WalletService } from './wallet.service';
+
+@Module({
+  controllers: [WalletController],
+  providers: [WalletService],
+})
+export class WalletModule {}
+
+
+---
