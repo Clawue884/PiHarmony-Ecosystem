438ddbbeb41f4e41dd8cd3802ffce62bcebbf0bf
Kapten boneng <iwak01bogo@gmail.com>
2025-01-06 19:33:41 +0700

    Create nft.module.ts

---
+++ nft.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { NftService } from './nft.service';
+
+@Module({
+  providers: [NftService],
+  exports: [NftService],
+})
+export class NftModule {}
+
+
+---
