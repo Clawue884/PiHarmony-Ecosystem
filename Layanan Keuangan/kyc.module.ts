843c28566b093a7e11f1095bb17ea6b7b228dc4b
Kapten boneng <iwak01bogo@gmail.com>
2025-01-06 18:48:27 +0700

    Create kyc.module.ts

---
+++ Layanan Keuangan/kyc.module.ts
@@ -0,0 +1,12 @@
+
+import { Module } from '@nestjs/common';
+import { KycService } from './kyc.service';
+
+@Module({
+  providers: [KycService],
+  exports: [KycService],
+})
+export class KycModule {}
+
+
+---
